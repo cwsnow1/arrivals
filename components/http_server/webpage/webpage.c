39a640ec998c407ccd176c803d49ee4660a64ce3
Caleb Snow <snow.calebw@gmail.com>
2026-01-12 08:13:23 -0500

    Update defaults, make CMake generate compressed webpage

--- components/http_server/webpage/webpage.c
+++ components/http_server/webpage/webpage.c
@@ -4,6 +4,6 @@
 // In a separate file bc intellisense doesn't recognize #embed and it was annoying mes
 
 uint8_t index_html[] = {
-#embed "index.html.gz" // run minify.py first TODO: make CMake run this automatically
+#embed "index.html.gz"
 };
 size_t index_html_size = sizeof index_html;
